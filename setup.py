40b7e13751383d02bb8893bf7b6e8cec8b6d500c
jduncan <jduncan@redhat.com>
2018-12-01 19:22:08 -0500

    package name fail

--- setup.py
+++ setup.py
@@ -39,12 +39,12 @@ setup(
     install_requires=[
         'python_magic',
         'ipaddr',
-        'python_backports_lzma'
+        'backports.lzma'
     ],
     requires=[
         'python_magic',
         'ipaddr',
-        'python_backports_lzma'
+        'backports.lzma'
     ],
     package_dir={'': 'soscleaner'},
     test_suite='tests',
