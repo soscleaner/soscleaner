6b3219727acd210d2178c56eb9b0eb2f5e6892d8
jduncan <jduncan@redhat.com>
2018-12-08 14:22:35 -0500

    Bump version: 0.3.20 → 0.3.21

--- soscleaner/soscleaner.py
+++ soscleaner/soscleaner.py
@@ -47,7 +47,7 @@ class SOSCleaner:
         self.domains = list()
         self.domainname = None
         self.report_dir = '/tmp'
-        self.version = '0.3.20'
+        self.version = '0.3.21'
 
         """
         Network Obfuscation Information
