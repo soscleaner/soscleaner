f6b51d23fc15783de787f37af7d7bae2cb361308
jduncan <jduncan@redhat.com>
2018-12-10 20:30:53 -0500

    Bump version: 0.3.63 → 0.3.64

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 
 from setuptools import setup
 
-version = '0.3.63'
+version = '0.3.64'
 name = 'soscleaner'
 
 setup(
--- soscleaner/soscleaner.py
+++ soscleaner/soscleaner.py
@@ -47,7 +47,7 @@ class SOSCleaner:
         self.domains = list()
         self.domainname = None
         self.report_dir = '/tmp'
-        self.version = '0.3.63'
+        self.version = '0.3.64'
 
         """
         Network Obfuscation Information
