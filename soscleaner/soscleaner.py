6e7617ecc927db41b85aa72eff631ea4de60e5d4
jduncan <jduncan@redhat.com>
2018-12-20 17:04:08 -0500

    close on #71 [ci skip]

--- soscleaner/soscleaner.py
+++ soscleaner/soscleaner.py
@@ -612,9 +612,9 @@ class SOSCleaner:
         # If we found domains, we need to sub them all out cleanly
         # If not, we'll just return the line as it was because we made no changes
         if domain_found:
-            line = re.sub(o_domain, domain, line)
-            line = re.sub(o_hostname, hostname, line)
-            line = re.sub(o_domainname, domainname, line)
+            line = re.sub(domain, o_domain, line)
+            line = re.sub(hostname, o_hostname, line)
+            line = re.sub(domainname, o_domainname, line)
 
         return line
 
