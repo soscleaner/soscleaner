7edcb00f280c6d8d2401f7b0a7fb2285fd42254d
jduncan <jduncan@redhat.com>
2018-12-25 20:16:24 -0600

    fix test20 [ci skip]

--- test_soscleaner.py
+++ test_soscleaner.py
@@ -234,7 +234,7 @@ class SOSCleanerTests(unittest.TestCase):
         self.cleaner._create_dn_report()
         fh = open(self.cleaner.dn_report, 'r')
         x = fh.readlines()
-        self.assertTrue(self.cleaner.domainname in x[1])
+        self.assertTrue(self.cleaner.domainname in x[2])
 
     def test21_create_dn_report_none(self):
         self.cleaner._create_dn_report()
