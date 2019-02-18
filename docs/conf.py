be638e6c089904e42e8428dacce62ce0179486ec
jduncan <jduncan@redhat.com>
2019-02-18 16:18:25 -0500

    bumping to test 0.3.96

--- docs/conf.py
+++ docs/conf.py
@@ -25,9 +25,9 @@ copyright = u'2019, Jamie Duncan'
 author = u'Jamie Duncan'
 
 # The short X.Y version
-version = u'0.3.95'
+version = u'0.3.96'
 # The full version, including alpha/beta/rc tags
-release = u'0.3.95'
+release = u'0.3.96'
 
 
 # -- General configuration ---------------------------------------------------
