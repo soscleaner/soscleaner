9a7c1f67e3e9c37fdb6f3f648e1474d3d8d345d1
jduncan <jduncan@redhat.com>
2019-02-18 22:12:06 -0500

    should be 100% covered now [ci-skip]

--- soscleaner/soscleaner.py
+++ soscleaner/soscleaner.py
@@ -695,8 +695,9 @@ class SOSCleaner:
                 add_domain = _eval_domains(root_domain)
                 if add_domain:
                     self.logger.debug("Found new subdomain of %s - %s", root_domain, domainname)
-                    o_domain = self._dn2db(root_domain, add_domain=True)
                     found_domain = True
+                    o_domain = self._dn2db(root_domain, add_domain=True)
+
 
         elif domain_depth == 2:
             o_domain = self.dn_db.get(hostname)
